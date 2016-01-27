2ce7c188c68420f6bff8c866d62eb34b1bf075d0
App Academy Student <appacademy@Apps-Mac-mini-109.local>
2016-01-27 11:13:09 -0500

    Add api namespace, course index route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root to: "sessions#new"
+  root to: "static_pages#root"
 
   resources :users, only: [:new, :create, :show]
   resource :session, only: [:new, :create, :destroy]
+
+  namespace :api, defaults: {format: :json} do
+    resources :courses, only: [:index]
+  end
 end
