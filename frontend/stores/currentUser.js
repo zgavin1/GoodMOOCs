216dd586c9affc588f45b909d99c8da24e0dfb02
Zach Gavin <gavinzach@gmail.com>
2016-01-28 21:19:29 -0500

    Add constant and store file for user for react.

---
+++ frontend/stores/currentUser.js
@@ -0,0 +1,34 @@
+var Store = require('flux/utils').Store;
+var AppDispatcher = require('../dispatcher/dispatcher');
+
+var _current_user = {};
+var CurrentUserStore = new Store(AppDispatcher);
+var CurrentUserConstants = require('../constants/current_user_constants');
+
+var _currentUserHasBeenFetched = false;
+
+CurrentUserStore.currentUser = function () {
+  return $.extend({}, _currentUser);
+};
+  
+CurrentUserStore.isLoggedIn = function () {
+  return !!_currentUser.id;
+};
+
+CurrentUserStore.userHasBeenFetched = function () {
+  return _currentUserHasBeenFetched;
+};
+
+CurrentUserStore.__onDispatch = function (payload) {
+  if (payload.actionType === CurrentUserConstants.RECEIVE_CURRENT_USER) {
+    // do stuff
+    //maybe more stuff
+
+
+    _currentUserHasBeenFetched = true;
+    _currentUser = payload.currentUser;
+    CurrentUserStore.__emitChange();
+  }
+};
+  
+module.exports = CurrentUserStore;
