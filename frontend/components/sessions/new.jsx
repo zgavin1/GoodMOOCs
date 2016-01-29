fac25f4b2ebc52a0dc1e318d4e9b1b3f18ac9956
Zach Gavin <gavinzach@gmail.com>
2016-01-28 22:55:08 -0500

    Building up the Session react component.

--- frontend/components/sessions/new.jsx
+++ frontend/components/sessions/new.jsx
@@ -3,8 +3,40 @@ var SessionsApiUtl = require('./../../util/sessions_api_util');
 
 var Session = React.createClass({
 	render: function () {
-		//
+		return (
+			<form onSubmit>
+
+			</form>
+			)
 	}
 });
 
 module.exports = Session;
+
+<h2>Sign In</h2>
+
+
+
+// My new session form from rails authentication
+
+<form action="<%= session_url %>" method="post">
+  <%= auth_token_input %>
+  <input type="hidden" name="user[username]" value="password">
+  <input type="hidden" name="user[password]" value="password">
+  <button>Demo User</button>
+</form>
+
+<br><br>
+<form action="<%= session_url %>" method="post">
+  <%= auth_token_input %>
+
+
+  <input type="text" name="user[username]" value="" placeholder="Username">
+
+  <br>
+  <input type="password" name="user[password]" value="" placeholder="Password">
+  <br>
+  <button>Submit</button>
+</form>
+
+<%= render 'users/form' %>
